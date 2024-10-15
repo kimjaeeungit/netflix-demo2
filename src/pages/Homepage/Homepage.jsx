e48e108b6d43511a6d0b03239e6c2dc114192b48
Jaeeun Kim <dmjfhe12@gmail.com>
2024-10-15 20:48:54 +0100

    UI 수정

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './components/Banner/Banner';
+import PopularMovieSlide from './components/PopularMovieSlide/PopularMovieSlide';
 
 // 1.배너 => popular movie를 들고와서 첫번쨰 아이템을 보여주기
 // 2. popular movie
@@ -9,6 +10,7 @@ const Homepage = () => {
   return (
     <div>
       <Banner />
+      <PopularMovieSlide />
     </div>
   );
 };
